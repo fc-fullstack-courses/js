de52f9551e706c0d5e56b546e1703139d54cc3b3
Kyrylo <fresh.js.ftd.@gmail.com>
2022-08-29 20:49:11 +0300

    Add basic operators

---
+++ assets/js/operators.js
@@ -0,0 +1,50 @@
+const result = (2 + 2) * 2;
+console.log(123);
+/*
+1+1
+1-1
+1*1
+1/1
+1%1
+1** 1
+
+*/
+let num = 5;
+// num = num + 1;
+console.log('=inc decrement=')
+console.log(num);
+// num++;
+// console.log(num);
+// ++num;
+// console.log(num);
+console.log(++num);
+console.log(num++);
+console.log(num);
+// ++num++;
+
+let num2 = 10;
+// num2++;
+// num2++;
+// num2++;
+// num2++;
+// num2++;
+// num2 = num2 + 5;
+num2 += 5;
+
+const x1 = 5;
+const x2 = 10;
+const res = x1 * x2;
+console.log(num2);
+
+let value = -5;
+let strange = true;
+
+console.log(+true);
+
+let verdict = true;
+
+/*
+  ! - приводит значение к логическому и инвертирует его
+  !! - приводит значение к логическому
+  console.log(!!value);
+*/
