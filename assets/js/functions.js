5794e6480ea9c1dc502b0609e97424867d46cd16
Kyrylo <fresh.js.ftd.@gmail.com>
2022-08-30 20:49:14 +0300

    Add sum of two numbers example

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,22 @@
 const data = console.log(123453523); // всегда undefined
 
-const name = prompt('Enter your name:'); // string
+// const name = prompt('Enter your name:'); // string
 
-const greetingMessage = 'Hello ' + name;
+// const greetingMessage = 'Hello ' + name;
 
-alert(greetingMessage);
+// alert(greetingMessage);
+
+
+// const num1 = +prompt('Enter first number');
+// const num2 = Number(prompt('Enter second number'));
+
+const num1 = prompt('Enter first number');
+const num2 = prompt('Enter second number');
+
+const result = +num1 + Number(num2);
+
+const sumMessage = num1 + ' + ' + num2 + ' = ' + result;
+// const message = num1 + ' + ' + num2 + ' = ' + (+num1 + Number(num2));
+alert(sumMessage);
+
+// alert(num1 + ' + ' + num2 + ' = ' + (+num1 + Number(num2)));
