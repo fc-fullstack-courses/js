a598701df56bf4570b10c5a2a1e3eaeb3776f3c6
Kyrylo <fresh.js.ftd.@gmail.com>
2022-08-30 18:45:00 +0300

    Add solution

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -9,17 +9,17 @@ console.log(123);
 1** 1
 
 */
-let num = 5;
+// let num = 5;
 // num = num + 1;
-console.log('=inc decrement=')
-console.log(num);
-// num++;
+console.log('=inc decrement=');
 // console.log(num);
-// ++num;
+// // num++;
+// // console.log(num);
+// // ++num;
+// // console.log(num);
+// console.log(++num);
+// console.log(num++);
 // console.log(num);
-console.log(++num);
-console.log(num++);
-console.log(num);
 // ++num++;
 
 let num2 = 10;
@@ -48,3 +48,34 @@ let verdict = true;
   !! - приводит значение к логическому
   console.log(!!value);
 */
+
+//создать две переменные и вывести в консоль резутьатт их умножения
+const a1 = 10;
+const a2 = 50;
+console.log(a1 * a2);
+// создать две переменные и вывести в консоль результат их деления
+const a3 = 10;
+const a4 = 50;
+console.log(a3 / a4);
+// создать две переменные и вывести в консоль резутьатт остатка от деления
+const a5 = 10;
+const a6 = 50;
+console.log(a5 % a6);
+//создайте переменную со строкой "0x500" и выведите в консоль результат
+//её приведения к числу
+const str = '0x500';
+console.log(+str);
+// создайте переменную со значением 300 / 5 + 27 * -3 + 21  и выведите
+// в консоль результат её приведения к булевому значению
+const expression = 300 / 5 + 27 * -3 + 21;
+console.log(!!expression);
+// Сократите следующие записи с помощью использования операторов присвоения
+
+let num = 10;
+num += 5;
+num /= 3;
+num *= 50;
+num -= 15;
+
+console.log(num);
+// выведите в консоль значение num
