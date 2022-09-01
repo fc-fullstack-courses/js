6ebebad46eb7a24c0252bdf4b7699e0bc2600587
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-01 20:18:18 +0300

    Add switch ... case

--- assets/js/conditionals.js
+++ assets/js/conditionals.js
@@ -1,31 +1,5 @@
 const getSumOfTwo = function (num1, num2) {
-  // if (isNaN(num1) || isNaN(num2)) {
-  //   alert('vse ploxo');
-  //   return NaN;
-  // } else {
-  //   // const result = ;
-
-  //   return num1 + num2;
-  // }
-  
-  // if (isNaN(num1)) {
-  //   alert('vse ploxo');
-  //   return NaN;
-  // }
-
-  // if(isNaN(num2)) {
-  //   alert('vse ploxo');
-  //   return NaN;
-  // }
-
-  // return num1 + num2;
-  // if (!isNaN(num1) && !isNaN(num2)) {
-
-  //   return  num1 + num2;
-  // } else {
-  //   alert('vse ploxo');
-  //   return NaN;
-  // }
+  // debugger;
   if (!isNaN(num1)) {
     if (!isNaN(num2)) {
       return num1 + num2;
@@ -37,40 +11,27 @@ const getSumOfTwo = function (num1, num2) {
 };
 
 // if ... else
-if (2 > 10) {
-  const test = 1;
-
-  if (5 > 1) {
-  }
-} else if (2 > 1) {
-  const test2 = 2;
-} else if (2 > -100) {
-  const test3 = 3;
-} else {
-}
 // switch case
 
-const friendName = 'Anton';
-
-const badFriend = 'Anton';
-const bestFriend = 'Test';
-const justFriend = 'UNdefined';
-
-if (friendName === bestFriend) {
-  // условие 1
-  console.log('Heelo my best friend');
-} else if (friendName === justFriend) {
-  //условие 2
-  console.log('Hi');
-
-  // if() {
-  //   if( ) {
-  //     if() {
-
-  //     }
-  //   }
-  // }
-} else {
-  // все остальные случаи
-  console.log('...');
+const input = prompt('Choose your fighter: \n1-Sub-Zero\n2-Scorpion\n3-Raiden');
+// debugger;
+switch (input) {
+  case '1':
+  case 1: {
+    alert('You picked Sub-Zero');
+    break; // останавливает выполнение
+  }
+  case '2': // ничего не сделал и пошел дальше
+  case 2: {
+    alert('You picked Scorpion');
+    break;
+  }
+  case '3':
+  case 3: {
+    alert('You picked Raiden');
+    break;
+  }
+  default:
+    alert('This fighter doesnt exist');
 }
+// input === 1
