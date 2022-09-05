903be099f786fc1c62cd2e85b5c224c2c1b3579a
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-05 20:35:04 +0300

    Add object CRUD

--- assets/js/object.js
+++ assets/js/object.js
@@ -3,7 +3,7 @@ const obj = {
   lineHeiht: 1.5,
   opacity: 0,
   'background color': 'red',
-  1: 4551
+  1: 4551,
 };
 
 const phone = {
@@ -13,4 +13,29 @@ const phone = {
     // method
     alert('Switched On');
   },
+  cpu: {
+    model: 'Qualcom Snapdragon 730',
+    frequency: 4,
+    frequencyUnit: 'GHz',
+  },
+  test: 'test',
 };
+// CREATE
+const obj1 = {}; // literal, BEST
+const obj2 = Object(); // BAD
+const obj3 = new Object(); // BAD
+// READ
+console.log(phone);
+// alert(phone);
+// READ object values
+console.log(phone.color);
+console.log(phone.cpu);
+console.log(phone.cpu.frequency);
+console.log(phone.ljnhgifdhtgreiufhdifhdguyfdgd); // undefined
+// UPDATE property
+phone.color = 'black';
+// Add property after creation
+// let battery = 4000;
+phone.battery = 4000;
+//DELETE
+delete phone.test;
