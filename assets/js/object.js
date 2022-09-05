b4974794fa511db8b91cf8b0312ef8a88c2bf37e
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-05 20:10:24 +0300

    Add obj example

---
+++ assets/js/object.js
@@ -0,0 +1,16 @@
+const obj = {
+  display: 'flex',
+  lineHeiht: 1.5,
+  opacity: 0,
+  'background color': 'red',
+  1: 4551
+};
+
+const phone = {
+  color: 'blue',
+  model: 'Moto G Plus',
+  switchOn: function () {
+    // method
+    alert('Switched On');
+  },
+};
