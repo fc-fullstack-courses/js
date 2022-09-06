51a0f705ccd12ef86606293741115dc8a41a0a5f
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-06 19:15:20 +0300

    Add function constructor example

--- assets/js/object.js
+++ assets/js/object.js
@@ -1,10 +1,10 @@
-const obj = {
-  display: 'flex',
-  lineHeiht: 1.5,
-  opacity: 0,
-  'background color': 'red',
-  1: 4551,
-};
+// const obj = {
+//   display: 'flex',
+//   lineHeiht: 1.5,
+//   opacity: 0,
+//   'background color': 'red',
+//   1: 4551,
+// };
 
 const phone = {
   color: 'blue',
@@ -20,6 +20,7 @@ const phone = {
   },
   test: 'test',
 };
+// CRUD
 // CREATE
 const obj1 = {}; // literal, BEST
 const obj2 = Object(); // BAD
@@ -37,5 +38,97 @@ phone.color = 'black';
 // Add property after creation
 // let battery = 4000;
 phone.battery = 4000;
-//DELETE
-delete phone.test;
+// phone.battery = {
+//   amount: 4000,
+//   unit: 'mAh',
+// };
+// DELETE
+// delete phone.test;
+// ctrl + /
+
+const obj = {
+  // key - string || Symbol
+  // value - any
+  // key : value,
+  // key2: value2,
+  // method: function () {}
+};
+
+const message = {
+  author: 'Ivan Vlasov',
+  text: 'методы?',
+  date: '17:54', // data курильщика
+  // date: new Date(2022,09,06,17,54)
+  test: {
+    value: {
+      data: 12345,
+    },
+  },
+  kfbkdsdksj: 'sdsafda',
+};
+
+const message2 = {
+  author: 'Ирина Мазур',
+  text: 'фунуция',
+  date: '17:52', // data курильщика
+  // date: new Date(2022,09,06,17,52)
+};
+
+console.log(message.author);
+console.log(message);
+console.log(message.test.value.data);
+
+/*
+  создать 2 -3 обьекта пользователей
+  у пользователей должны быть
+  имя
+   фамилия
+   возраст
+   функция чистки зубов
+*/
+// const user1 = {
+//   firstName: 'Test',
+//   lastName: 'Testenko',
+//   age: 42,
+//   brushTeeth: function () {
+//     console.log('Burshing teeth ...');
+//   },
+// };
+
+// const user2 = {
+//   firstName: 'Null',
+//   lastName: 'Nullovich',
+//   age: 50,
+//   brushTeeth: function () {
+//     console.log('Burshing teeth ...');
+//   },
+// };
+
+// const user3 = {
+//   firstname: 'Undefined',
+//   lastnAme: 'Undefined',
+//   age: 15,
+//   brushTeeth: function () {
+//     console.log('Burshing teeth ...');
+//   },
+// };
+
+// функция - конструктор
+const User = function (firstName, lastName, age, email) {
+  // debugger;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.email = email;
+
+  this.brushTeeth = function () {
+    console.log('Burshing teeth ...');
+  };
+
+  return false; // в режиме конструктора игнорируется
+};
+// const CompanyName = function () {}
+const user1 = new User('Test', 'Testenko', 42, 'test@test.test');
+const user2 = new User();
+const user4 = new User('Anton', 'Antonovich', 12, 'test1@test.test'); // правильно
+// const user5 = User('Anton', 'Antonovich', 12); // неправильный вызов
