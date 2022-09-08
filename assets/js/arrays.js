936343e81ad29cbee0cd479a88ce274498754ae3
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-08 18:57:37 +0300

    Add push, pop, unshift, shift examples

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -6,13 +6,13 @@ test4@tes.test
 test5@tes.test
 */
 
-const userEmails = {
-  0: "test1@tes.test",
-  1: "test2@tes.test",
-  2: "test3@tes.test",
-  3: "test4@tes.test",
-  4: "test5@tes.test",
-};
+// const userEmails = {
+//   0: "test1@tes.test",
+//   1: "test2@tes.test",
+//   2: "test3@tes.test",
+//   3: "test4@tes.test",
+//   4: "test5@tes.test",
+// };
 
 // const userEmailsArr = new Array(
 //   "test1@tes.test",
@@ -20,7 +20,31 @@ const userEmails = {
 //   "test3@tes.test"
 // );
 
-const arr = [true, 10, 'test'];
-const emptyArr = [];
+// const arr = [true, 10, 'test'];
+// const emptyArr = [];
+// // const arr = [10, userEmails];
 
-const value = arr[2]; // получение значения из массива
+// const value = arr[2]; // получение значения из массива
+// arr[0] = 20; // измение значения в массиве
+// arr[2] = 500; // добавляет значение если его нет
+// // arr[30] = 1000; // плохо, можно ошибится
+// // arr.test = 'test'; // плохо
+// arr[arr.length] = 1000;
+
+const userNames = ["Test", "Null", "Anton", "Pavlo", "Olena"];
+
+// ложит вищи в конец массива
+userNames.push("Anton");
+userNames.push("Kyrylo");
+let newArrLength = userNames.push("Mefodiy", "Ivan", "Nastya");
+
+// удаляет последний єлемент из массива и сохраняет его
+const deletedFromEnd = userNames.pop();
+// userNames.pop();
+
+// ложит вищи в начало массива
+userNames.unshift("Felix");
+newArrLength = userNames.unshift("John", "Jane");
+
+// удаляет первый єлемент из массива и сохраняет его
+const deletedFromStart = userNames.shift();
