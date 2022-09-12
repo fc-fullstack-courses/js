d6670db74b7c69a6bb7977c959093ac44749a553
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-12 20:38:47 +0300

    Add isOn

--- assets/js/inheritance.js
+++ assets/js/inheritance.js
@@ -1,7 +1,8 @@
 'use strict';
-function Phone(model, manufacturer) {
+function Phone(model, manufacturer, isOn) {
   this.model = model;
   this.manufacturer = manufacturer;
+  this.isOn = isOn;
 }
 
 const phoneProto = {
@@ -35,8 +36,8 @@ Phone.prototype = phoneProto;
 // phone1.__proto__ = phoneProto;
 // phone2.__proto__ = phoneProto;
 
-const phone1 = new Phone('Pro', 'Apple');
-const phone2 = new Phone('Basic', 'Google');
+const phone1 = new Phone('Pro', 'Apple', false);
+const phone2 = new Phone('Basic', 'Google', true);
 
 const arr1 = [1];
 const arr2 = [1];
