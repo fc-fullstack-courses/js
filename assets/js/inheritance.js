a3ae58da0da45d405f3e4ec537ebe2b869a8465d
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-12 20:03:24 +0300

    Add prototype chain

--- assets/js/inheritance.js
+++ assets/js/inheritance.js
@@ -6,21 +6,30 @@ function Phone(model) {
   };
 }
 
-const phone1 = {
-  model: 'Pro',
-};
-
-const phone2 = {
-  model: 'Basic',
-};
-
 const phoneProto = {
   call: function () {
     console.log(1);
   },
 };
 
-phone1.__proto__ = phoneProto;
+const deviceProto = {
+  turnOn: function () {
+    console.log('device is turned on now');
+  },
+};
+
+phoneProto.__proto__ = deviceProto;
+
+const phone1 = {
+  model: 'Pro',
+  __proto__: phoneProto,
+};
+
+const phone2 = {
+  model: 'Basic',
+};
+
+// phone1.__proto__ = phoneProto;
 phone2.__proto__ = phoneProto;
 
 // const phone1 = new Phone('Pro');
