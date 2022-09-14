a7bbfa52e1a9ef11630b3870180e08af8c44398a
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-14 19:20:20 +0300

    Add remaining string methods

--- assets/js/globalObjects.js
+++ assets/js/globalObjects.js
@@ -1,3 +1,4 @@
+// =================== STRING ==================
 const str1 = 'testes';
 
 // console.log(str1[0]);
@@ -6,7 +7,7 @@ const str1 = 'testes';
 // console.log(str1[3]);
 
 for (let i = 0; i < str1.length; i++) {
-  console.log(str1[i]);
+  // console.log(str1[i]);
 }
 
 str1.charAt(3); // аналог str1[3]
@@ -20,5 +21,20 @@ const isSpaceInString = str1.includes(' ');
 const firstE = str1.indexOf('e');
 
 const str3 = str1.replace('e', 'x');
+// const str3 = str1.replaceAll('est', 'x');
 
-const str4 = str1.slice(1, 3);
+const str4 = str1.slice(5, 3);
+console.log(str4);
+const str5 = str1.substring(5, 3);
+console.log(str5);
+
+const capsStr = str1.toUpperCase();
+
+const test = '                                         email@mail.com       ';
+const test1 =
+  '                                         Test Testovich         Testenko       ';
+const trimmedString = test1.trim();
+
+const preArrayString = 'Ivan Stefanovich Petrenko';
+const array = preArrayString.split(' ', 1);
+const postArrayString = array.join('-');
