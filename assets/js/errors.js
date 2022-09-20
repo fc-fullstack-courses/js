5b9a620f2745bc9caafa087ee7f8e433991f26a3
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-20 19:48:21 +0300

    Add try ... catch

--- assets/js/errors.js
+++ assets/js/errors.js
@@ -8,22 +8,34 @@ function multiply(num1, num2) {
     return num1 * num2;
   }
   // все плохо
-  throw new TypeError('Incorrect parameters, expected two numbers');
+  throw new RangeError('Incorrect parameters, expected two numbers');
 }
 
 const user = {
   name: 'Test',
-  salary: 'three thousand',
+  salary: 3000,
   monthsOfPayment: 4,
 };
 
-const newPayment = multiply(user.salary, user.monthsOfPayment);
-
+try {
+  // пробуем выполнить опасный код
+  alert('try');
+  const newPayment = multiply(user.salary, user.monthsOfPayment); // опасный код
+  alert(`You have been payed ${newPayment} $`);
+} catch (err) {
+  // что делать в случае ошибки
+  alert('catch');
+  if (err instanceof TypeError) {
+    alert('Bad type');
+  } else if (err instanceof RangeError) {
+    alert('range');
+  }
+} finally {
+  // код который выполнится в любом случае
+  alert('finally');
+}
 
 // throw 1235445;
-
-
-alert(`You have been payed ${newPayment} $`);
 
 /*
 Error - базовая ошибка
