2796ab536f32ba0b5006407821462f2486a69b5c
Kyrylo <fresh.js.ftd.@gmail.com>
2022-09-30 18:30:14 +0300

    Add basic map methods

--- assets/js/dataStructures.js
+++ assets/js/dataStructures.js
@@ -12,8 +12,8 @@ set1.add(null);
 set1.delete('test'); // удаляет конкретное значение
 set1.delete(arr);
 
-console.log(set1.has(1)); // узнаем есть ли значение в сете
-console.log(set1.has(10000));
+// console.log(set1.has(1)); // узнаем есть ли значение в сете
+// console.log(set1.has(10000));
 
 const arr1 = [5, 7, 8, 9, 3, 8, 8, 6];
 
@@ -57,26 +57,73 @@ const names2 = ['Feofan', 'Simon', 'Stepan', 'Roman', 'Anton'];
 const uniqueNames = [...new Set([...names1, ...names2])];
 const uniqueNames1 = new Array(...new Set([...names1, ...names2]));
 
+// for (const key in uniqueNames1) {
+//   console.log(key);
+// }
+
+// for (const value of uniqueNames1) {
+//   console.log(value);
+// }
+
 set3.forEach((value, valueAgain, set) => {
-  console.log(value);
-  console.log(valueAgain);
-  console.log(set);
+  // console.log(value);
+  // console.log(valueAgain);
+  // console.log(set);
 });
 
 const setIter = set3.values(); // итератор
 
 for (const item of setIter) {
-  console.log(item);
+  // console.log(item);
 }
 
 // для сета set.keys = set.values
 
 for (const item of set3.keys()) {
-  console.log(item);
+  // console.log(item);
 }
-
-// 
 
 for (const item of set3.entries()) {
-  console.log(item); // массив с однотипной структурой - кортеж
+  // console.log(item); // массив с однотипной структурой - кортеж
 }
+
+/*
+  1. ключом может быть ЧТО угодно
+  2. сохраняется порядок вставки
+  3. есть итератор
+  4. нет ключей кроме установленных вами
+  5. есть  свойство размера (size)
+  6. Map лучше работает в сценариях с частым добавлением и удалением пар ключ-значение.
+  7. преобразования для передачи по сети надо делать вручную
+*/
+
+const map = new Map();
+
+map.set('key', 'value');
+map.set(123, 456);
+map.set('123', '456');
+
+const func = () => {
+  console.log('Why...');
+};
+map.set(func, true);
+map.set(func, null);
+
+console.log(map.get(func));
+console.log(map.get(123));
+
+console.log(map.has(999));
+console.log(map.has(123));
+
+map.delete(123);
+
+const map2 = new Map([
+  ['key1', 'value1'],
+  ['key2', 'value2'],
+  ['key3', 'value3'],
+  ['key4', 'value4'],
+  ['key5', 'value5'],
+  ['key6', 'value6'],
+]);
+
+// map.clear();
